bd6fe2d1d8c42442dea1b4bb04a4bf912b6882d3
سید بنیامین خلیفه <nvioir@gmail.com>
2020-08-20 01:38:37 +0430

    add edit route file

--- app/routes/admin.php
+++ app/routes/admin.php
@@ -9,7 +9,7 @@ Route::get('admin/upload','controllers@fileController->uploadPage');
 Route::post('admin/upload','controllers@fileController->uploadFile');
 Route::post('admin/file/list','controllers@fileController->getList');
 Route::post('admin/file/remove','controllers@fileController->removeFile');
-// Route::post('admin/file/edit','controllers@fileController->uploadFile');
+Route::post('admin/file/edit','controllers@fileController->editFile');
 
 Route::post('admin/category/add','controllers@categoryController->add');
 Route::post('admin/category/remove','controllers@categoryController->remove');
