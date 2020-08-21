d302a159c110f20cbd75161c082e78fa118c8dce
سید بنیامین خلیفه <nvioir@gmail.com>
2020-08-21 15:06:40 +0430

    update

--- app/views/admin/upload.php
+++ app/views/admin/upload.php
@@ -74,9 +74,11 @@
     </div>
 
     <div class="uk-width-auto " style="margin-left:auto;">
-      <div style="width: 200px;">
-        <label>Search</label>
-        <input oninput="searchList($(this))" type="text" style="border-radius: 50px;" class="uk-input" name="" value="">
+      <div style="width: 220px;">
+        <form class="uk-search uk-search-default">
+          <span uk-search-icon></span>
+          <input  oninput="searchList($(this))" autocomplete="off"  style="background: white;" class="uk-search-input uk-input" type="search" placeholder="Search...">
+        </form>
       </div>
     </div>
 
@@ -120,234 +122,234 @@
 
 <script>
 
-  var category = 'content';
-  var storage  = 'public';
+var category = 'content';
+var storage  = 'public';
 
-  function setCategory(e) {
-    category = e.val();
-    upload.params['category']=category;
+function setCategory(e) {
+  category = e.val();
+  upload.params['category']=category;
 
-  }
+}
 
-  function setStorageType(e) {
-    storage = e.val();
-    upload.params['storage']=storage;
-  }
-
-
-  var bar = document.getElementById('js-progressbar');
-
-  upload = UIkit.upload('.js-upload', {
-
-    url: '@url("admin/upload")',
-    name:'file',
-    multiple:true,
-    params:{
-      category:category,
-      storage:storage
-    },
-
-    beforeSend: function () {
-    },
-    beforeAll: function () {
-    },
-    load: function () {
-    },
-    error: function () {
-      console.log('error', arguments);
-      error_post()
-    },
-    complete: function () {
-    },
-
-    loadStart: function (e) {
-
-      bar.removeAttribute('hidden');
-      bar.max = e.total;
-      bar.value = e.loaded;
-    },
-
-    progress: function (e) {
-
-      bar.max = e.total;
-      bar.value = e.loaded;
-    },
-
-    loadEnd: function (e) {
-      console.log('loadEnd', arguments);
-
-      bar.max = e.total;
-      bar.value = e.loaded;
-    },
-
-    completeAll: function (get) {
-      console.log('completeAll',get, arguments);
-
-      setTimeout(function () {
-        bar.setAttribute('hidden', 'hidden');
-      }, 1000);
-
-      response = jQuery.parseJSON(get.response);
-      console.log(response);
-
-      if (response.ok) {
-        notifi_success();
-        console.log();
-        $('#select-get-list-category').val(upload.params.category).change();
-        $('#select-get-list-storage').val(upload.params.storage).change();
-        getFileList();
-      }
-      else {
-        UIkit.modal.alert(response.errors[0].msg);
-      }
-    }
-
-  });
+function setStorageType(e) {
+  storage = e.val();
+  upload.params['storage']=storage;
+}
 
 
-  var categoryList = 'content';
-  var storageList  = 'public';
+var bar = document.getElementById('js-progressbar');
 
-  function setCategoryList(e) {
-    categoryList = e.val();
-    getFileList();
-  }
+upload = UIkit.upload('.js-upload', {
 
-  function setStorageTypeList(e) {
-    storageList = e.val();
-    getFileList();
-  }
+  url: '@url("admin/upload")',
+  name:'file',
+  multiple:true,
+  params:{
+    category:category,
+    storage:storage
+  },
 
+  beforeSend: function () {
+  },
+  beforeAll: function () {
+  },
+  load: function () {
+  },
+  error: function () {
+    console.log('error', arguments);
+    error_post()
+  },
+  complete: function () {
+  },
 
-  function getFileList() {
-    post('@url("admin/file/list")',{
-      category:categoryList,
-      storage:storageList
-    },function (get) {
-      loading(false);
-      generateList(get.list)
-    })
-  }
+  loadStart: function (e) {
 
-  $(function () {
-    getFileList();
-  })
+    bar.removeAttribute('hidden');
+    bar.max = e.total;
+    bar.value = e.loaded;
+  },
 
-  function generateList(list) {
-    $('#file-items>div:not(#file-item-sample)').remove();
-    for(item of list){
-      cloneNewFile(item);
-    }
+  progress: function (e) {
+
+    bar.max = e.total;
+    bar.value = e.loaded;
+  },
+
+  loadEnd: function (e) {
+    console.log('loadEnd', arguments);
+
+    bar.max = e.total;
+    bar.value = e.loaded;
+  },
+
+  completeAll: function (get) {
+    console.log('completeAll',get, arguments);
 
     setTimeout(function () {
-      loading(false);
-    },500);
+      bar.setAttribute('hidden', 'hidden');
+    }, 1000);
 
-  }
+    response = jQuery.parseJSON(get.response);
+    console.log(response);
 
-  base_file_url = '{{url()}}';
-
-  function cloneNewFile(info) {
-    item = $('#file-item-sample').clone();
-    item.removeAttr('id').data('id',info.id);
-    item.find('.item-name').text(info.name);
-    item.find('.item-type').text(info.type+' - '+info.ext);
-
-    info.name = encodeURI(info.name);
-
-    if (info.location=='public') {
-      url = base_file_url+info.category+'/'+info.name;
+    if (response.ok) {
+      notifi_success();
+      console.log();
+      $('#select-get-list-category').val(upload.params.category).change();
+      $('#select-get-list-storage').val(upload.params.storage).change();
+      getFileList();
     }
     else {
-      url = base_file_url+'file/image/'+info.category+'/'+info.name;
+      UIkit.modal.alert(response.errors[0].msg);
     }
+  }
 
-    item.data('url',url);
-    item.find('input').val(url)
+});
 
-    if (info.type.indexOf('image')>-1) {
-      item.find('.item-image>i').fadeOut(0);
-      item.find('.item-image>img').fadeIn(0).attr('src',url);
+
+var categoryList = 'content';
+var storageList  = 'public';
+
+function setCategoryList(e) {
+  categoryList = e.val();
+  getFileList();
+}
+
+function setStorageTypeList(e) {
+  storageList = e.val();
+  getFileList();
+}
+
+
+function getFileList() {
+  post('@url("admin/file/list")',{
+    category:categoryList,
+    storage:storageList
+  },function (get) {
+    loading(false);
+    generateList(get.list)
+  })
+}
+
+$(function () {
+  getFileList();
+})
+
+function generateList(list) {
+  $('#file-items>div:not(#file-item-sample)').remove();
+  for(item of list){
+    cloneNewFile(item);
+  }
+
+  setTimeout(function () {
+    loading(false);
+  },500);
+
+}
+
+base_file_url = '{{url()}}';
+
+function cloneNewFile(info) {
+  item = $('#file-item-sample').clone();
+  item.removeAttr('id').data('id',info.id);
+  item.find('.item-name').text(info.name);
+  item.find('.item-type').text(info.type+' - '+info.ext);
+
+  info.name = encodeURI(info.name);
+
+  if (info.location=='public') {
+    url = base_file_url+info.category+'/'+info.name;
+  }
+  else {
+    url = base_file_url+'file/image/'+info.category+'/'+info.name;
+  }
+
+  item.data('url',url);
+  item.find('input').val(url)
+
+  if (info.type.indexOf('image')>-1) {
+    item.find('.item-image>i').fadeOut(0);
+    item.find('.item-image>img').fadeIn(0).attr('src',url);
+  }
+
+  item.css('display','');
+
+  $('#file-items').append(item);
+}
+
+function copyUrl(i) {
+  console.log('copy run');
+  var input = i.closest('.item-in-card-main').find('input');
+  input.css('display','');
+
+  copyText = input.get(0);
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  document.execCommand("copy");
+  input.css('display','none');
+
+  notifi_success('Copyed.');
+}
+
+
+
+function deleteFileMessage(i){
+  var main = i.closest('.item-in-card-main');
+
+  id = main.data('id');
+  name = main.find('.item-name').text();
+
+  delete_message({id:id,name:name},"Delete '"+name+"' file?",deleteFile);
+}
+
+function deleteFile(data) {
+  post('@url("admin/file/remove")',data,function (get) {
+    if (get.ok) {
+      notifi_success();
+      getFileList();
     }
+  })
+}
 
-    item.css('display','');
+function searchList(input) {
+  $("#file-items .item-in-card-main:not(#file-item-sample)").filter(function() {
+    item = $(this).find('.item-name').text().toLowerCase().indexOf(input.val());
+    $(this).toggle(item > -1)
+  });
+}
 
-    $('#file-items').append(item);
+function editNameMessage(btn) {
+
+  item = btn.closest('.item-in-card-main');
+  _edit_item_id = item.data('id');
+  name = item.find('.item-name').text();
+  UIkit.modal.prompt('Edit :',name).then(function (input) {
+    editName(input,name,_edit_item_id)
+  })
+}
+
+function editName(name,old,id) {
+
+  console.log(old,name,_edit_item_id);
+
+  if (name==null || name =='' || name == old) {
+    return;
   }
 
-  function copyUrl(i) {
-    console.log('copy run');
-    var input = i.closest('.item-in-card-main').find('input');
-    input.css('display','');
+  post('@url("admin/file/edit")',{
+    name:name,
+    id:id
+  },function (get) {
+    if (get.ok) {
 
-    copyText = input.get(0);
+      notifi_success();
 
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+      getFileList();
 
-    document.execCommand("copy");
-    input.css('display','none');
-
-    notifi_success('Copyed.');
-  }
-
-
-
-  function deleteFileMessage(i){
-    var main = i.closest('.item-in-card-main');
-
-    id = main.data('id');
-    name = main.find('.item-name').text();
-
-    delete_message({id:id,name:name},"Delete '"+name+"' file?",deleteFile);
-  }
-
-  function deleteFile(data) {
-    post('@url("admin/file/remove")',data,function (get) {
-      if (get.ok) {
-        notifi_success();
-        getFileList();
-      }
-    })
-  }
-
-  function searchList(input) {
-    $("#file-items .item-in-card-main:not(#file-item-sample)").filter(function() {
-      item = $(this).find('.item-name').text().toLowerCase().indexOf(input.val());
-      $(this).toggle(item > -1)
-    });
-  }
-
-  function editNameMessage(btn) {
-
-    item = btn.closest('.item-in-card-main');
-    _edit_item_id = item.data('id');
-    name = item.find('.item-name').text();
-    UIkit.modal.prompt('Edit :',name).then(function (input) {
-      editName(input,name,_edit_item_id)
-    })
-  }
-
-  function editName(name,old,id) {
-
-    console.log(old,name,_edit_item_id);
-
-    if (name==null || name =='' || name == old) {
-      return;
     }
-
-    post('@url("admin/file/edit")',{
-      name:name,
-      id:id
-    },function (get) {
-      if (get.ok) {
-
-        notifi_success();
-
-        getFileList();
-
-      }
-    });
-  }
+  });
+}
 
 </script>
--- app/views/layout/loading.php
+++ app/views/layout/loading.php
@@ -1,5 +1,5 @@
 <div id="modal-loading" class="uk-flex-top" uk-modal="bg-close:false;esc-close:false;">
-  <div class="uk-modal-dialog uk-margin-auto-vertical uk-text-center modal-loading-style">
+  <div class="uk-modal-dialog uk-margin-auto-vertical uk-text-center modal-loading-style" style="background: transparent;">
     <div uk-spinner="ratio: 3"></div>
     <div class="uk-text-center">
       <span id="modal-loading-text"></span>
@@ -14,7 +14,7 @@
     if (typeof action === 'string' || action instanceof String) {
       modal.find('#modal-loading-text').text(action);
       UIkit.modal('#modal-loading').show();
-    } 
+    }
     else if (typeof action === "boolean" && action ) {
       modal.find('#modal-loading-text').text('');
       UIkit.modal('#modal-loading').show();
