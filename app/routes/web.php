8030169696ede118f478f8540022917a917475c0
سید بنیامین خلیفه <nvioir@gmail.com>
2020-08-21 15:02:18 +0430

    add login route

--- app/routes/web.php
+++ app/routes/web.php
@@ -4,3 +4,7 @@ use webrium\core\Route;
 
 Route::get('','controllers@indexController->index');
 Route::get('file/image/content/*','controllers@fileController->downloadFile');
+
+
+Route::get('login','controllers@userController->loginPage');
+Route::post('login','controllers@userController->loginUser');
