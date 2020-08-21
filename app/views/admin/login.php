7f5b9d002a74b2ba98015885e2d23b6f24025272
سید بنیامین خلیفه <nvioir@gmail.com>
2020-08-21 15:07:14 +0430

    login view

---
+++ app/views/admin/login.php
@@ -0,0 +1,66 @@
+<script src="@url('library/jquery/jquery-3.3.1.min.js')"></script>
+<script src="@url('js/public.js')"></script>
+
+<link rel="stylesheet" href="@url('css/login.css')">
+
+
+<div class="uk-flex uk-flex-middle uk-flex-center" style="background: rgb(22, 149, 149);" uk-height-viewport="expand: true">
+  <div class="uk-width-large uk-card uk-card-body uk-card-default uk-card-hover">
+    <div class="">
+
+      <form id="form"  method="post">
+
+
+        <div class="uk-margin">
+          <h4 class="uk-text-center color-orange uk-text-bold" style="color: #99948b;" >Login User</h4>
+        </div>
+
+        <div class="uk-margin">
+          <label>Username</label>
+          <input type="text" class="uk-input" name="username" value="">
+        </div>
+
+        <div class="uk-margin">
+          <label>Password</label>
+          <input type="password" class="uk-input" name="password" value="">
+        </div>
+
+        <div class="uk-margin uk-margin-medium-top">
+          <div class="uk-child-width-1-2@m" uk-grid>
+            <div >
+              <div class="">
+                <img src="{!! $cbuilder->inline(); !!}" />
+              </div>
+            </div>
+
+            <div class="">
+              <div class="">
+                <input type="text" class="uk-input" name="captcha" value="">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="uk-margin uk-margin-medium-top">
+          <button onclick="login()" type="button" class="uk-width-1-1 uk-button uk-button-primary">Login</button>
+        </div>
+
+      </form>
+
+    </div>
+  </div>
+</div>
+
+
+<script type="text/javascript">
+  function login(){
+
+    data = new FormData(document.getElementById('form'));
+
+    ajax('@url("login")',data,function (get) {
+      if (get.ok) {
+        window.location.href='@url("admin")';
+      }
+    });
+  }
+</script>
