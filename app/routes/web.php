a53b16a794afaf6f06a16e003e2c80ee5cd54438
سید بنیامین خلیفه <nvioir@gmail.com>
2020-08-25 23:42:00 +0430

    add captcha route

--- app/routes/web.php
+++ app/routes/web.php
@@ -6,6 +6,9 @@ Route::get('','controllers@indexController->index');
 Route::get('file/image/content/*','controllers@fileController->downloadFile');
 Route::get('profile/image/*','controllers@fileController->showProfileImage');
 
+// change captcha
+Route::post('captcha/new','controllers@userController->captcha');
+
 
 Route::get('login','controllers@userController->loginPage');
 Route::post('login','controllers@userController->loginUser');
