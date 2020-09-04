d86d9a9f7b3f027674bf6fdc9643cef0ae90fe97
سید بنیامین خلیفه <nvioir@gmail.com>
2020-09-04 11:58:54 +0430

    update

--- app/controllers/postController.php
+++ app/controllers/postController.php
@@ -2,21 +2,38 @@
 namespace app\controllers;
 
 use app\models\Panel;
+use app\models\Post;
 use app\models\Categorys;
 
 class postController
 {
 
-  public function postAdd(){
+  public function postManage($id=false){
+
+    $post = false;
+
+    if ($id!=false) {
+      $post = Post::getById($id);
+    }
 
     $categorys = Categorys::getAll();
 
-    return Panel::view('post-add',[
+    return Panel::view('post-manage',[
       'categorys'=>$categorys,
-      'title'=>'Add Post'
+      'title'=>($post?'Edit':'New' ).' Post',
+      'post'=>$post
     ]);
   }
 
+  public function postEditPage(){
+    return $this->postManage(input('id'));
+  }
+
+  //
+  public function postAdd(){
+
+    return Post::save();
+  }
 
 
 }
