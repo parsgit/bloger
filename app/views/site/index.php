67ef63a1c37fc21718aacdc5c22a4228f483f061
سید بنیامین خلیفه <nvioir@gmail.com>
2020-09-24 17:41:51 +0330

    edit index page and add new post list

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -9,10 +9,36 @@
         {!! $index->text_1->data !!}
       </div>
 
-      <div class="row justify-content-center mt-5">
+
+
+      <div class="mt-5">
+        <h3>New Posts</h3>
+      </div>
+
+      <div class="row justify-content-center mt-3">
+
+        @foreach($posts->posts as $post)
+        <div class="col-4 mt-2">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center"> <a href="{{$post->url}}">{{$post->title}}</a> </h5>
+
+            </div>
+
+          </div>
+        </div>
+
+        @endforeach
+
+      </div>
+
+      <div class="mt-5">
+        <h3>Items</h3>
+      </div>
+      <div class="row justify-content-center mt-3">
 
         @foreach($index->items->params as $item)
-        <div class="col-4">
+        <div class="col-4 mt-2">
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">{{$item[0]->data}}</h5>
@@ -25,6 +51,7 @@
         @endforeach
 
       </div>
+
     </div>
 
   </div>
