cf861ae8c22f23677350cb460caaed34e7b9ff30
سید بنیامین خلیفه <nvioir@gmail.com>
2020-09-24 17:17:40 +0330

    show index view

--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -1,13 +1,17 @@
 <?php
 namespace app\controllers;
 
+use app\models\Post;
+use app\models\Settings;
+
 class indexController
 {
 
 
   public function index()
   {
-    echo "...";
+    $index = Settings::config('index');
+    return Post::view('index',['index'=>$index]);
   }
 
 }
